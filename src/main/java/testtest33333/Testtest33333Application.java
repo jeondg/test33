803a2aaefb11a84471ea6c8c134cdb1879c6395c
Donggeon Jeon <idwjsehdrjs@naver.com>
2025-03-24 10:09:51 +0900

    first commit

---
+++ src/main/java/testtest33333/Testtest33333Application.java
@@ -0,0 +1,13 @@
+package testtest33333;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testtest33333Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testtest33333Application.class, args);
+	}
+
+}
---
+++ src/test/java/testtest33333/Testtest33333ApplicationTests.java
@@ -0,0 +1,13 @@
+package testtest33333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testtest33333ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
